d410d03982fa084709572e5137baff1dcee87741
황선혜 <ghkdtjs6451@gmail.com>
2020-07-05 14:54:56 +0900

    전화걸기(DAIL)  기능 완성....

--- app/src/main/java/kr/co/tjoeun/intent_20200705/AndroidIntent_20200705.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200705/AndroidIntent_20200705.kt
@@ -1,7 +1,10 @@
 package kr.co.tjoeun.intent_20200705
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_android_intent_20200705.*
 
 class AndroidIntent_20200705 : AppCompatActivity() {
@@ -11,6 +14,20 @@ class AndroidIntent_20200705 : AppCompatActivity() {
 
 //        다이얼버튼 이벤트
         dialBtn.setOnClickListener {
+
+//            입력된 전화번호를 받아오자.
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+//            안드로이드에게, 어디에 전화걸지 정보 전달 => Uri class
+//            전화 Uri 양식 - tel:010-0000-0000
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+//            Log.d("String 가공확인", "tel:${inputPhoneNum}")
+
+//            전화 화면으로 이동하는 Intent
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+            startActivity(myIntent)
 
 
         }
